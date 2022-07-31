06114afd719af3c38f0fdb2daa6a002482ea86a0
Ruslan_Astratov <r.astratov@thecoders.ru>
2022-07-31 19:01:02 +0300

    в api  поправил код для eslint

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,8 @@
-import http from "../utils/axiosConfig";
+import http from '../utils/axiosConfig'
 
-export const getWaterItems = () =>
-  http.get(process.env.REACT_APP_MOCK_API_WATER_ITEMS);
+export const getWaterItems = () => http.get(process.env.REACT_APP_MOCK_API_WATER_ITEMS)
 
 export const sendQuickOrder = (data) =>
-  http.post(process.env.REACT_APP_MOCK_API_SEND_QUICK_ORDER, data);
+  http.post(process.env.REACT_APP_MOCK_API_SEND_QUICK_ORDER, data)
 
-export const sendOrder = (data) =>
-  http.post(process.env.REACT_APP_MOCK_API_SEND_ORDER, data);
+export const sendOrder = (data) => http.post(process.env.REACT_APP_MOCK_API_SEND_ORDER, data)
