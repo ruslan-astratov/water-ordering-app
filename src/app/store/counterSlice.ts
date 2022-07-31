2c18d5d4e2ab759ad7ca258dea5135b5719f6dee
Ruslan_Astratov <r.astratov@thecoders.ru>
2022-07-31 11:11:13 +0300

    удалил папку features

--- src/app/store/counterSlice.ts
+++ src/app/store/counterSlice.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState, AppThunk } from "./store";
-import { fetchCount } from "../../features/counter/counterAPI";
 
 import { getWaterItems } from "../../api/api";
 
@@ -43,20 +42,6 @@ const initialState: CounterState = {
   selectedSliderItem: {},
   isOpenModalSuccess: false,
 };
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-export const incrementAsync = createAsyncThunk(
-  "counter/fetchCount",
-  async (amount: number) => {
-    const response = await fetchCount(amount);
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
-  }
-);
 
 export const fetchSliderItems = createAsyncThunk(
   "counter/sliderItems",
